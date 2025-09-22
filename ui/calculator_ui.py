e256faaf5a0d27f5f1d1ef2ec470e3aec7ccfaa0
Shayan Kermani <shayan.ker85@gmail.com>
2025-09-22 15:06:29 +0330

    Changed UI

--- ui/calculator_ui.py
+++ ui/calculator_ui.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 from ui.error_ui import Error
 import math
 import re
@@ -13,37 +15,58 @@ class CalculatorUI(QWidget):
         
     def create_ui(self):
         main_layout = QVBoxLayout()
-        
+        main_layout.setContentsMargins(10, 10, 10, 10)
+        main_layout.setSpacing(10)
+
         self.display = QLineEdit()
         self.display.setReadOnly(True)
-        self.display.setFixedHeight(40)
-        self.display.setStyleSheet("font-size: 20px; padding: 5px;")
+        self.display.setFixedHeight(60)
+        self.display.setFont(QFont("Helvetica", 24))
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setStyleSheet("""
+            background-color: #222;
+            color: white;
+            border-radius: 10px;
+            padding-right: 15px;
+        """)
         main_layout.addWidget(self.display)
-        
+
         buttons_layout = QGridLayout()
-        
+        buttons_layout.setSpacing(8)
+
         buttons = [
-            "C/CE", '+/-', '√', '%',
+            'C', '+/-', '√', '%',
             '7', '8', '9', '/',
             '4', '5', '6', '*',
             '1', '2', '3', '-',
             '0', '.', '=', '+'
         ]
-        
+
+        self.buttons = {}
         row, col = 0, 0
-        for button_text in buttons:
-            button = QPushButton(button_text)
-            button.setFixedSize(60, 60)
-            button.setStyleSheet("font-size: 18px;")
-            button.clicked.connect(lambda checked, bt=button_text: self.button_clicked(bt))
-            buttons_layout.addWidget(button, row, col)
+        for btn_text in buttons:
+            btn = QPushButton(btn_text)
+            btn.setFixedSize(70, 70)
+            btn.setFont(QFont("Helvetica", 18))
+            btn.setStyleSheet(self.button_style(btn_text))
+            btn.clicked.connect(lambda checked, b=btn_text: self.button_clicked(b))
+            buttons_layout.addWidget(btn, row, col)
+            self.buttons[btn_text] = btn
             col += 1
             if col > 3:
                 col = 0
                 row += 1
-        
+
         main_layout.addLayout(buttons_layout)
         self.setLayout(main_layout)
+
+    def button_style(self, btn_text):
+        if btn_text in ['C', '+/-', '√']:
+            return "background-color: #555; color: white; border-radius: 15px;"
+        elif btn_text in ['/', '*', '-', '+', '=']:
+            return "background-color: #ff9500; color: white; border-radius: 15px;"
+        else:
+            return "background-color: #333; color: white; border-radius: 15px;"
 
     def button_clicked(self, button_text):
         text = self.display.text()
@@ -70,7 +93,7 @@ class CalculatorUI(QWidget):
                 else:
                     new_number = "-" + number
                 self.display.setText(text[:int(start)] + new_number)
-        elif button_text == 'C/CE':
+        elif button_text == 'C':
             self.display.setText('')
         else:
             self.display.setText(text + button_text)
