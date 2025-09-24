08765ad20e0bd7818d3e9d09cd43e02b05b54c5b
Shayan Kermani <shayan.ker85@gmail.com>
2025-09-24 13:15:48 +0330

    Fix square root calculation problem

--- logic/calculator_logic.py
+++ logic/calculator_logic.py
@@ -3,9 +3,10 @@ import re
 
 def change_eval_symbols(text):
     if '√' in text:
+        text = text.replace('√', 'sqrt(')
         text = re.sub(r"sqrt\((\d+)", r"sqrt(\1)", text)
     if '÷' in text:
-        text = text.replace("÷", '/')
+        text = text.replace('÷', '/')
     if '×' in text:
         text = text.replace('×', '*')
     return text
